c566964bfb20ea3dabe5ba6101d054fd8db481fe
João Lenon <lenonsec7@gmail.com>
2024-06-13 12:55:42 +0100

    feat: use object to perform actions

--- src/agents.py
+++ src/agents.py
@@ -1,3 +1,5 @@
+from src.constants import ACTION
+
 def simple_agent(observation):
     position, velocity = observation
 
@@ -5,7 +7,7 @@ def simple_agent(observation):
     # to the left or starting to come up the
     # hill, go to the right (action=2).
     if -0.1 < position < 0.4:
-        return 2
+        return ACTION.RIGHT
 
     # If the car is going to the left hand side hill
     # we can't counteract the momentum going to the
@@ -13,10 +15,9 @@ def simple_agent(observation):
     # to the left (action=1) until gravity pushes the
     # car down the hill.
     if velocity < 0 and position < -0.2:
-        # Go to the left
-        return 0
+        return ACTION.LEFT
 
     # If any of the above requirements met, the car will
     # not perform any action. The only thing that will
     # make the car move at this point is the momentum.
-    return 1
+    return ACTION.STOP
--- src/constants.py
+++ src/constants.py
@@ -1,1 +1,6 @@
 ENV_NAME = 'MountainCar-v0'
+
+class ACTION:
+    LEFT = 0
+    STOP = 1
+    RIGHT = 2
